a9fdeac79162b4ff3ffa62ce7faa270f3659ee35
Jack Rhodes <95jr022@gmail.com>
2025-09-20 17:45:12 +0100

    Basic Shadcn UI + override

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,229 @@
+"use client";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { add, format } from "date-fns";
 import Image from "next/image";
+import { useState } from "react";
+
+type PlugState = "unplugged" | "plugged-in";
+
+type CarState = {
+  model: string;
+  nickname: string;
+  stateOfCharge: number;
+};
+
+type ScheduledCharge = {
+  startTime: Date;
+  endTime: Date;
+  targetChargePercent: number;
+};
+
+type OverrideCharge = {
+  startTime: Date;
+  endTime: Date;
+};
+
+type ChargerIdleState = {
+  status: "idle";
+};
+
+type ChargerScheduledState = {
+  status: "awaiting-scheduled-charge" | "charging-scheduled";
+  charge: ScheduledCharge;
+};
+
+type ChargerOverrideState = {
+  status: "charging-override";
+  charge: OverrideCharge;
+};
+
+type ChargerState =
+  | ChargerIdleState
+  | ChargerScheduledState
+  | ChargerOverrideState;
+
+const OVERRIDE_TIMER_DURATION = { minutes: 60 };
+const dateFormatString = "p";
+
+const formatChargePercentage = (input: number) => {
+  return input.toFixed(1);
+};
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [plugState, setPlugState] = useState<PlugState>("unplugged");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const unplugCar = () => setPlugState("unplugged");
+  const plugInCar = () => setPlugState("plugged-in");
+
+  const [carState, setCarState] = useState<CarState>({
+    model: "Mini Cooper E",
+    nickname: "Herbi-E",
+    stateOfCharge: 69.0,
+  });
+
+  const [chargingState, setChargingState] = useState<ChargerState>({
+    status: "idle",
+  });
+
+  const triggerOverride = () => {
+    const now = new Date();
+    setChargingState({
+      status: "charging-override",
+      charge: {
+        startTime: now,
+        endTime: add(now, OVERRIDE_TIMER_DURATION),
+      },
+    });
+  };
+
+  const cancelOverride = () => {
+    setChargingState({
+      status: "idle",
+    });
+  };
+
+  return (
+    <div className="space-y-6 p-4">
+      <h1 className="text-xl font-bold">Charge control panel</h1>
+      <div className="space-y-8">
+        <div className="space-y-4 bg-secondary p-4 rounded shadow">
+          <h2 className="text-l font-bold">Charger state</h2>
+          <div className="space-y-2">
+            <div className="flex flex-row gap-2">
+              <div>Status:</div>
+              {
+                {
+                  "plugged-in": <Badge variant={"default"}>Plugged in</Badge>,
+                  unplugged: <Badge variant={"outline"}>Unplugged</Badge>,
+                }[plugState]
+              }
+            </div>
+            <div className="flex flex-row gap-2">
+              <Button
+                variant={"outline"}
+                onClick={unplugCar}
+                disabled={plugState === "unplugged"}
+              >
+                Unplug
+              </Button>
+              <Button onClick={plugInCar} disabled={plugState === "plugged-in"}>
+                Plug in
+              </Button>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="space-y-1">
+          <h2 className="text-l font-bold">Car state</h2>
+          <div>{carState.nickname}</div>
+          <div>{carState.model}</div>
+          <div>
+            SoC: {formatChargePercentage(carState.stateOfCharge)}% charged
+          </div>
+        </div>
+        <div className="space-y-1">
+          <h2 className="text-l font-bold">Charging state</h2>
+          {
+            {
+              "plugged-in": (
+                <>
+                  <div className="flex flex-row gap-2">
+                    <div>Status:</div>
+                    {
+                      {
+                        idle: <Badge>Idle</Badge>,
+                        "awaiting-scheduled-charge": <Badge>Waiting</Badge>,
+                        "charging-scheduled": (
+                          <Badge>Charging (scheduled)</Badge>
+                        ),
+                        "charging-override": <Badge>Charging (override)</Badge>,
+                      }[chargingState.status]
+                    }
+                  </div>
+                  <div>
+                    {chargingState.status === "idle" && (
+                      <div>Not much to see here!</div>
+                    )}
+
+                    {chargingState.status === "awaiting-scheduled-charge" && (
+                      <div>
+                        <p>
+                          {`Charge scheduled for ${format(
+                            chargingState.charge.startTime,
+                            dateFormatString
+                          )}.`}
+                        </p>
+                        <p>
+                          {`Aiming for ${
+                            chargingState.charge.targetChargePercent
+                          } by ${format(
+                            chargingState.charge.endTime,
+                            dateFormatString
+                          )}`}
+                        </p>
+                      </div>
+                    )}
+
+                    {chargingState.status === "charging-scheduled" && (
+                      <div>
+                        <p>
+                          {`Charging since ${format(
+                            chargingState.charge.startTime,
+                            dateFormatString
+                          )}.`}
+                        </p>
+                        <p>
+                          {`Aiming for ${
+                            chargingState.charge.targetChargePercent
+                          } by ${format(
+                            chargingState.charge.endTime,
+                            dateFormatString
+                          )}`}
+                        </p>
+                      </div>
+                    )}
+
+                    {chargingState.status === "charging-override" && (
+                      <div>
+                        <p>
+                          {`Charging since ${format(
+                            chargingState.charge.startTime,
+                            dateFormatString
+                          )}.`}
+                        </p>
+                        <p>
+                          {`Charging override active until ${format(
+                            chargingState.charge.endTime,
+                            dateFormatString
+                          )}`}
+                        </p>
+                      </div>
+                    )}
+                  </div>
+
+                  <div className="flex flex-row gap-2">
+                    <Button
+                      onClick={triggerOverride}
+                      disabled={chargingState.status === "charging-override"}
+                    >
+                      Override
+                    </Button>
+                    <Button
+                      onClick={cancelOverride}
+                      disabled={chargingState.status !== "charging-override"}
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                </>
+              ),
+              unplugged: "Charging state not available while car is unplugged",
+            }[plugState]
+          }
+        </div>
+      </div>
     </div>
   );
 }
